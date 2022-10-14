eacce07f4cbb189018b2846b84e337a51a4bcc43
박주형 <105642704+qkrwngud@users.noreply.github.com>
2022-10-14 12:08:07 +0900

    2022.10.14

---
+++ 10.14/selection sort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main(){
+	int a, b, min, temp;
+	
+	int c[5] = {5, 2, 8, 3, 1};
+	
+	for(a = 0; a < 4; a ++)
+	{
+		min=a;
+		for(b = a + 1; b < 5; b ++)
+		{
+			if(c[b] < c[min]) min = b;
+		}
+		temp = c[a];
+		c[a] = c[min];
+		c[min]=temp;
+		
+		printf("\n%d회전\n", a + 1);
+		for(b = 0; b < 5; b ++)
+			printf("%3d", c[b]);
+	}
+}
