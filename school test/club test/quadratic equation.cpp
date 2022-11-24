2c09914272686c5185881ce6dd94882d6ace9e92
박주형 <105642704+qkrwngud@users.noreply.github.com>
2022-11-24 13:53:57 +0900

    2022.11.24

---
+++ school test/club test/quadratic equation.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+// 이차방정식
+int main() {
+
+	cout << "ax2 + bx + c = 0\n";
+	
+	double a = 0, b = 0, c = 0;
+
+	cout << "a입력: ";
+	cin >> a;
+
+	cout << "b입력: ";
+	cin >> b;
+
+	cout << "c입력: ";
+	cin >> c;
+
+	const double discriminant = b * b - 4 * a * c;
+
+	double root = sqrt(discriminant);
+	
+	cout << endl;
+	
+	if (a == 0)
+	{
+		cout << "x = " << - (c / b);
+	}
+	else if (discriminant > 0)
+	{
+		cout << "서로 다른 두근\nx = " << (-b + root) / (2 * a) << " or " << "x = " << (-b - root) / (2 * a);
+	}
+	else if (discriminant == 0)
+	{
+		cout <<"중근\nx = " << (-b + root) / (2 * a);
+	}
+	else
+	{
+		cout << "근이 없음";
+	}
+	cout << endl;
+}
